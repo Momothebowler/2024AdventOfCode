990775f885a29044c720f1e0944db85532bd6426
Momothebowler <momothebowler@gmail.com>
2024-12-05 07:35:25 -0500

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -65,24 +65,11 @@ def part2():
                 temp.append(i[j + 1])
         updated.append(temp)
 
-    updated2 = []
-    for i in updated:
-        temp = []
-        for j in range(len(i) - 1):
-            if i[j + 1] in dict[i[j]]:
-                temp.append(i[j])
-            else:
-                shift = len(temp)
-                for k in range(len(i) - shift):
-                    if i[j] != "" and i[k + shift] in dict[i[j]]:
-                        temp.append(i[j])
-                        i[j] = ""
-
-            if j == len(i) - 1:
-                temp.append(i[j + 1])
-        updated2.append(temp)
+    updated = updates
+    updated = sorted(updates, key=dict.get)
+    print(updated)
     sum = 0
-    for i in updated2:
+    for i in updated:
         sum += int(i[int((len(i) - 1) / 2)])
     print(sum)
 
